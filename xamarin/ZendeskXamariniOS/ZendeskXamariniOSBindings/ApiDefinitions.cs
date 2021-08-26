f0f32f8c88dd1f7cc41c39d0e1cb5bb1134e2a9e
Niall Kelly <niallkelly808@gmail.com>
2021-08-26 12:51:24 +0100

    removing unused class from bindings and fixing issue with CommonUI SDK embedding

--- xamarin/ZendeskXamariniOS/ZendeskXamariniOSBindings/ApiDefinitions.cs
+++ xamarin/ZendeskXamariniOS/ZendeskXamariniOSBindings/ApiDefinitions.cs
@@ -68,37 +68,4 @@ namespace ZendeskiOS {
 	[BaseType(typeof(NSObject))]
 	interface ZDKConfiguration {
 	}
-
-	// @interface ZDKConfigurations : NSObject
-	[BaseType(typeof(NSObject))]
-	[DisableDefaultCtor]
-	interface ZDKConfigurations {
-		// @property (readonly, copy, nonatomic) NSArray<id<ZDKConfiguration>> * _Nonnull configs;
-		[Export("configs", ArgumentSemantic.Copy)]
-		ZDKConfiguration[] Configs { get; }
-
-		// -(instancetype _Nonnull)initWithConfigs:(NSArray<id<ZDKConfiguration>> * _Nonnull)configs __attribute__((objc_designated_initializer));
-		[Export("initWithConfigs:")]
-		[DesignatedInitializer]
-		IntPtr Constructor(ZDKConfiguration[] configs);
-
-		[Export("startIndex")]
-		nint StartIndex { get; }
-
-		// @property (readonly, nonatomic) NSInteger endIndex;
-		[Export("endIndex")]
-		nint EndIndex { get; }
-
-		// -(void)insert:(id<ZDKConfiguration> _Nonnull)configuration;
-		[Export("insert:")]
-		void Insert(ZDKConfiguration configuration);
-
-		// -(id<ZDKConfiguration> _Nonnull)objectAtIndexedSubscript:(NSInteger)index __attribute__((warn_unused_result("")));
-		[Export("objectAtIndexedSubscript:")]
-		ZDKConfiguration ObjectAtIndexedSubscript(nint index);
-
-		// -(void)addDefaultConfigIfNeeded:(id<ZDKConfiguration> _Nonnull)config;
-		[Export("addDefaultConfigIfNeeded:")]
-		void AddDefaultConfigIfNeeded(ZDKConfiguration config);
-	}
 }
